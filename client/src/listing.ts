1e2a1b73973b2db82d15d1ca35b36a6c9f67979a
sheddy-bx <shadrack@bx.studio>
2025-03-25 13:19:56 +0100

    fix: update BASE_URL to production endpoint

--- client/src/listing.ts
+++ client/src/listing.ts
@@ -1,5 +1,5 @@
 import type { Listing } from "../../types/Listing";
-const BASE_URL = "https://hudson-properties-dev.vercel.app";
+const BASE_URL = "https://hudson-properties.vercel.app";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const listingSlugId = new URLSearchParams(window.location.search).get("id");
